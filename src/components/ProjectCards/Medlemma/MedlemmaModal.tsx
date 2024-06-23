952d10cb938ce1e709e3744023399039e9d963f9
gnagetEmil <emil@gnaget.se>
2024-06-23 05:24:16 +0200

    Added more info to the project modals

--- src/components/ProjectCards/Medlemma/MedlemmaModal.tsx
+++ src/components/ProjectCards/Medlemma/MedlemmaModal.tsx
@@ -1,52 +1,59 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Text,
-    Link,
-    ButtonGroup,
-    Flex,
-  } from '@chakra-ui/react';
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Link,
+  Badge,
+  ButtonGroup,
+  Flex,
+  Stack,
+} from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
+
+interface SmartHouseoModalProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const PortfolioModal: React.FC<SmartHouseoModalProps> = ({ isOpen, onClose }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent ml={2} mr={2}>
+          <ModalHeader>Medlemma</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+            An app for managing your store memberships. With this app, users can easily collect, organize, and manage all their memberships in various stores efficiently. Users can also smoothly display their membership cards directly from their mobile when needed.</Text>
+            <Stack marginTop="4" direction="row" wrap="wrap" spacing={2}>
+              <Badge colorScheme="purple">Kotlin</Badge>
+              <Badge colorScheme="blue">Jetpack Compose</Badge>
+              <Badge colorScheme="red">Andriod Studio</Badge>
+              <Badge colorScheme="pink">Firebase</Badge>
+            </Stack>
+          </ModalBody>
   
-  interface MedlemmaModalProps {
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
-  }
-  
-  const MedlemmaModal: React.FC<MedlemmaModalProps> = ({ isOpen, onClose }) => {
-    return (
-      <>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Medlemma</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <Text>An app for managing your store memberships. With this app, users can easily collect, organize, and manage all their memberships in various stores efficiently. Users can also smoothly display their membership cards directly from their mobile when needed.</Text>
-            </ModalBody>
-  
-            <ModalFooter>
-            <ButtonGroup spacing='2'>
-                          <Link href='https://github.com/JacobssonEmil' isExternal>
-                          <Flex>
-                              <FaGithub size="30"/>
-                                <Link marginLeft={2} >Github</Link>
-                              </Flex>
-                          </Link>
-                      </ButtonGroup>
-              
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  }
-  
-  export default MedlemmaModal;
-  
+          <ModalFooter>
+            <ButtonGroup spacing="2">
+              <Link href='https://github.com/JacobssonEmil' isExternal>
+                <Flex align="center">
+                  <FaGithub size="30"/>
+                  <Text marginLeft="2">Github</Text>
+                </Flex>
+              </Link>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default PortfolioModal;
--- src/components/ProjectCards/Portfolio/PortfolioModal.tsx
+++ src/components/ProjectCards/Portfolio/PortfolioModal.tsx
@@ -1,52 +1,60 @@
 import {
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    ModalCloseButton,
-    Text,
-    Link,
-    ButtonGroup,
-    Flex,
-  } from '@chakra-ui/react';
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Text,
+  Link,
+  Badge,
+  ButtonGroup,
+  Flex,
+  Stack,
+} from '@chakra-ui/react';
 import { FaGithub } from 'react-icons/fa';
+
+interface SmartHouseoModalProps {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const PortfolioModal: React.FC<SmartHouseoModalProps> = ({ isOpen, onClose }) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent ml={2} mr={2}>
+          <ModalHeader>Portfolio</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>
+            A portfolio website designed to showcase my projects.</Text>
+            <Stack marginTop="4" direction="row" wrap="wrap" spacing={2}>
+              <Badge colorScheme="purple">TypeScript</Badge>
+              <Badge colorScheme="blue">React</Badge>
+              <Badge colorScheme="red">ChakraUI</Badge>
+              <Badge colorScheme="pink">React</Badge>
+              <Badge colorScheme="yellow">CSS</Badge>
+            </Stack>
+          </ModalBody>
   
-  interface PortfolioModalProps {
-    isOpen: boolean;
-    onOpen: () => void;
-    onClose: () => void;
-  }
-  
-  const PortfolioModal: React.FC<PortfolioModalProps> = ({ isOpen, onClose }) => {
-    return (
-      <>
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Portfolio</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody>
-              <Text>An app for managing your store memberships. With this app, users can easily collect, organize, and manage all their memberships in various stores efficiently. Users can also smoothly display their membership cards directly from their mobile when needed.</Text>
-            </ModalBody>
-  
-            <ModalFooter>
-            <ButtonGroup spacing='2'>
-                          <Link href='https://github.com/JacobssonEmil' isExternal>
-                          <Flex>
-                              <FaGithub size="30"/>
-                                <Link marginLeft={2} >Github</Link>
-                              </Flex>
-                          </Link>
-                      </ButtonGroup>
-              
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    );
-  }
-  
-  export default PortfolioModal;
-  
+          <ModalFooter>
+            <ButtonGroup spacing="2">
+              <Link href='https://github.com/JacobssonEmil' isExternal>
+                <Flex align="center">
+                  <FaGithub size="30"/>
+                  <Text marginLeft="2">Github</Text>
+                </Flex>
+              </Link>
+            </ButtonGroup>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+export default PortfolioModal;
